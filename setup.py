8044927ff3fb1132bec7acc125ea603520aa0215
Tomer Klein <tomer.klein@gmail.com>
2025-12-26 22:57:39 +0000

    Update version number

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ with open("README.md", "r", encoding="utf-8") as fh:
 
 setup(
     name="py-zerobyte",
-    version="1.1.0",
+    version="1.2.0",
     author="Tomer Klein",
     author_email="tomer.klein@gmail.com",
     description="Python SDK for Zerobyte API - Manage volumes, backups, and snapshots",
